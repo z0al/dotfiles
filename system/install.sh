112c3a16643dbd1aa9f915d8a50be2eec0e2cf66
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-09-01 22:02:17 +0200

    feat: fix BIOS clock

--- system/install.sh
+++ system/install.sh
@@ -5,3 +5,8 @@ sudo apt-get -y install build-essential git curl vim snapd
 
 # https://github.com/Mayccoll/Gogh dependencies
 sudo apt-get -y install dconf-cli uuid-runtime
+
+
+# Fix BIOS Clock
+# https://support.system76.com/articles/dual-booting/#fix-your-clock
+timedatectl set-local-rtc 1 --adjust-system-clock
