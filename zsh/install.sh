727401c991892c0ded2cf35bf4133aff8b61129a
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-09-06 15:13:30 +0200

    refactor: merge "system" into "zsh"

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo apt-get -y install zsh curl
+# Essentials
+sudo apt-get -y install zsh curl fonts-firacode vim 
 
 # antibody
 curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
