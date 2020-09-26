33a132158ab27efc78a9c786a5d7ce9adc8d3bf2
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-09-26 16:35:59 +0200

    install pulseaudio

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Essentials
-sudo apt-get -y install zsh curl fonts-firacode vim 
+sudo apt-get -y install zsh curl fonts-firacode vim pulseaudio
 
 # antibody
 curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
