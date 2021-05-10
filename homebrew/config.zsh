628903f8135a857da7f4cad8ae60c5319f781de2
z0ali <ah.tajelsir@gmail.com>
2021-05-10 07:27:00 +0200

    chore: remove homebrew

--- homebrew/config.zsh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-# https://docs.brew.sh/Homebrew-on-Linux
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
--- homebrew/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Forces the installation script to not wait for user input
-export CI=true
-
-# See https://docs.brew.sh/Homebrew-on-Linux
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
