d91cf8ddac50ef1e28a85384c71b1d146ff8371f
z0al <z0al@users.noreply.github.com>
2022-11-19 12:08:04 +0100

    chore(home/term): add SVG icon for Kitty

--- overlays/kitty/default.nix
+++ overlays/kitty/default.nix
@@ -2,7 +2,7 @@ channels: final: prev: {
   kitty = channels.unstable.kitty.overrideAttrs (o: {
     postInstall = (o.postInstall or "") + ''
       cp ${./kitty.png} $out/share/icons/hicolor/256x256/apps/kitty.png
-      rm $out/share/icons/hicolor/scalable/apps/kitty.svg
+      cp ${./kitty.svg} $out/share/icons/hicolor/scalable/apps/kitty.svg
     '';
   });
 }
