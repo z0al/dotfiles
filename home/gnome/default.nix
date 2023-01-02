bdb72e5ee561a887f4f419266915be32de9dffcd
Ahmed T. Ali <12673605+z0al@users.noreply.github.com>
2023-01-02 10:51:02 +0100

    chore(gnome): update desktop preference
    Signed-off-by: Ahmed T. Ali <12673605+z0al@users.noreply.github.com>

--- home/gnome/default.nix
+++ home/gnome/default.nix
@@ -8,6 +8,12 @@
     settings = {
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
+        clock-show-date = true;
+        clock-show-weekday = true;
+      };
+
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,close";
       };
     };
   };
