52f12844898338fb8e480e2035fd79840ce4ebda
z0al <z0al@users.noreply.github.com>
2023-01-04 23:37:07 +0100

    chore(shell): fix starship nix-shell prompt

--- home/term/starship.nix
+++ home/term/starship.nix
@@ -13,6 +13,13 @@
       cmd_duration = {
         format = "[$duration]($style)";
       };
+
+      nix_shell = {
+        format = "in [$symbol$state(\\($name\\))]($style) ";
+        symbol = "❄️ ";
+        impure_msg = "";
+        pure_msg = "pure ";
+      };
     };
   };
 }
