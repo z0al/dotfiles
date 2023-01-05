d47b30ed554762cb2517f4c546c4d3f37cf315f3
z0al <z0al@users.noreply.github.com>
2023-01-05 22:47:27 +0100

    chore(term): add fish plugins

--- home/term/shell/fish.nix
+++ home/term/shell/fish.nix
@@ -1,4 +1,11 @@
+{ pkgs, ... }:
+
 {
+  home.packages = with pkgs.fishPlugins; [
+    sponge
+    autopair-fish
+  ];
+
   programs.fish = {
     enable = true;
 
@@ -9,5 +16,17 @@
       # Bind CTRL+Backspace to delete a word
       bind \b backward-kill-word
     '';
+
+    plugins = [
+      {
+        name = "history-merge";
+        src = pkgs.fetchFromGitHub {
+          owner = "2m";
+          repo = "fish-history-merge";
+          rev = "7e415b8ab843a64313708273cf659efbf471ad39";
+          sha256 = "oy32I92sYgEbeVX41Oic8653eJY5bCE/b7EjZuETjMI=";
+        };
+      }
+    ];
   };
 }
