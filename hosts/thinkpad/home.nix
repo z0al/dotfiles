f7ae1510c2803ea14eb8c092f310d9fa740fc203
Ahmed T. Ali <12673605+z0al@users.noreply.github.com>
2023-01-16 20:09:39 +0100

    chore(thinkpad): autostart slack

--- hosts/thinkpad/home.nix
+++ hosts/thinkpad/home.nix
@@ -16,4 +16,11 @@
   programs.chromium.extensions = [
     { id = "glnpjglilkicbckjpbgcfkogebgllemb"; } # Okta
   ];
+
+  # Autostart Slack in the background
+  xdg.configFile."autostart/slack.desktop" = {
+    text = builtins.replaceStrings
+      [ "slack %U" ] [ "slack -u %U" ]
+      (builtins.readFile "${pkgs.slack}/share/applications/slack.desktop");
+  };
 }
