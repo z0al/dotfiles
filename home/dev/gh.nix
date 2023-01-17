90062bfab8fdebc1b7c586006879c813d8b1465d
z0al <z0al@users.noreply.github.com>
2023-01-17 21:26:30 +0100

    fix(dev): prefer ssh git protocol

--- home/dev/gh.nix
+++ home/dev/gh.nix
@@ -10,6 +10,9 @@
     enable = true;
 
     settings = {
+      # Works well with SSH auth via 1Password
+      git_protocol = "ssh";
+
       aliases = {
         clone = "repo clone";
         co = "pr checkout";
