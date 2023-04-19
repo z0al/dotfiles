721354e800cf2be10959f11407aab72b829983e0
Ahmed T. Ali <12673605+z0al@users.noreply.github.com>
2023-04-19 16:45:58 +0200

    chore(1password): persist only relevant configs

--- home/apps/1password.nix
+++ home/apps/1password.nix
@@ -45,7 +45,11 @@ in
 
   home.persistence."/nix/data/home/${user}" = {
     directories = [
-      ".config/1Password"
+      ".config/1Password/settings"
+    ];
+
+    files = [
+      ".config/1Password/1password.sqlite"
     ];
   };
 }
