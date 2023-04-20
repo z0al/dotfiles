175d49f6099d11798339516d33d92f1907da7277
z0al <z0al@users.noreply.github.com>
2023-04-20 18:53:13 +0200

    chore(starship): use catppuccin theme

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -1,3 +1,43 @@
+let
+  # https://github.com/catppuccin/starship
+  catppuccin = {
+    character = {
+      success_symbol = "[❯](bold maroon)";
+      error_symbol = "[[✗](bold red) ❯](maroon)";
+    };
+
+    palette = "custom";
+    palettes.custom = {
+      rosewater = "#f5e0dc";
+      flamingo = "#f2cdcd";
+      pink = "#f5c2e7";
+      mauve = "#cba6f7";
+      red = "#f38ba8";
+      maroon = "#eba0ac";
+      peach = "#fab387";
+      yellow = "#f9e2af";
+      green = "#a6e3a1";
+      teal = "#94e2d5";
+      sky = "#89dceb";
+      sapphire = "#74c7ec";
+      blue = "#89b4fa";
+      lavender = "#b4befe";
+      text = "#cdd6f4";
+      subtext1 = "#bac2de";
+      subtext0 = "#a6adc8";
+      overlay2 = "#9399b2";
+      overlay1 = "#7f849c";
+      overlay0 = "#6c7086";
+      surface2 = "#585b70";
+      surface1 = "#45475a";
+      surface0 = "#313244";
+      base = "#1e1e2e";
+      mantle = "#181825";
+      crust = "#11111b";
+    };
+  };
+in
+
 {
   programs.starship = {
     enable = true;
@@ -5,7 +45,7 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
 
-    settings = {
+    settings = catppuccin // {
       battery = {
         disabled = true;
       };
@@ -35,6 +75,7 @@
       };
 
       directory = {
+        style = "bold lavender";
         read_only = " ";
       };
 
