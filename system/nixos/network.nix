369893c52b0ee4b03040cb04af6d8c0173028e81
z0al <z0al@users.noreply.github.com>
2023-04-21 17:12:38 +0200

    fix(nixos): frequent connection interruption

--- system/nixos/network.nix
+++ system/nixos/network.nix
@@ -1,6 +1,12 @@
 { user, ... }:
 
 {
-  networking.networkmanager.enable = true;
   users.users.${user}.extraGroups = [ "networkmanager" ];
+
+  networking = {
+    networkmanager.enable = true;
+
+    # Causes frequent "Your connection was interrupted" errors
+    enableIPv6 = false;
+  };
 }
