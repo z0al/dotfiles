91ab68a76e2a6b36ce25bac910f6f8522358c982
z0al <z0al@users.noreply.github.com>
2023-04-22 15:32:59 +0200

    fix(nixos): correctly set active hint radius

--- system/nixos/gnome/extensions/pop-shell.nix
+++ system/nixos/gnome/extensions/pop-shell.nix
@@ -1,9 +1,12 @@
+{ lib, ... }:
+
 {
   dconf.settings."org/gnome/shell/extensions/pop-shell" = {
     tile-by-default = true;
     show-title = false;
     gap-outer = 1;
     active-hint = true;
-    active-hint-border-radius = 12;
+    hint-color-rgba = "rgba(250, 179, 135, 1)";
+    active-hint-border-radius = lib.hm.gvariant.mkUint32 12;
   };
 }
