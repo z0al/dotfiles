db013a7cf5e26fcd5929cdab8c3b74b7b81f000a
z0al <z0al@users.noreply.github.com>
2023-05-10 19:18:33 +0200

    fix(nixos): persist /tmp

--- system/nixos/file-system.nix
+++ system/nixos/file-system.nix
@@ -43,6 +43,7 @@ in
         "/var/lib/nixos"
         "/var/lib/systemd/coredump"
         { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+        { directory = "/tmp"; mode = "u=rwx,g=rwx,o=rwx"; }
       ];
 
       users."${user}" = {
