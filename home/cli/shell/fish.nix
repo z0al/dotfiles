6ee65b67479aa6aa2f437d49f8d54f0b7a927085
z0al <z0al@users.noreply.github.com>
2023-06-22 05:00:15 +0200

    fix(fish): remove annoying history merge plugin

--- home/cli/shell/fish.nix
+++ home/cli/shell/fish.nix
@@ -27,18 +27,6 @@ in
     interactiveShellInit = ''
       any-nix-shell fish --info-right | source
     '';
-
-    plugins = [
-      {
-        name = "history-merge";
-        src = pkgs.fetchFromGitHub {
-          owner = "2m";
-          repo = "fish-history-merge";
-          rev = "7e415b8ab843a64313708273cf659efbf471ad39";
-          sha256 = "oy32I92sYgEbeVX41Oic8653eJY5bCE/b7EjZuETjMI=";
-        };
-      }
-    ];
   };
 
   d.fs.persisted = {
