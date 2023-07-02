1c9202adca63f0721511155bbbacacc747a118a3
z0al <z0al@users.noreply.github.com>
2023-07-02 14:30:20 +0200

    chore(nixos): bump root tmpfs to 4gb

--- system/nixos/file-system.nix
+++ system/nixos/file-system.nix
@@ -16,7 +16,7 @@ in
       "/" = {
         device = "none";
         fsType = "tmpfs";
-        options = [ "defaults" "size=2G" "mode=755" ];
+        options = [ "defaults" "size=4G" "mode=755" ];
       };
     };
 
