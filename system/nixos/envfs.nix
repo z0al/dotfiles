267e9f1a330b53355cb9c0ec4e8dc7afd11c0b54
z0al <z0al@users.noreply.github.com>
2023-07-28 21:06:04 +0200

    feat(nixos): enable fallback for /bin/bash

--- system/nixos/envfs.nix
+++ system/nixos/envfs.nix
@@ -1,6 +1,14 @@
+{ pkgs, ... }:
+
 {
   # https://github.com/Mic92/envfs
-  services.envfs.enable = true;
+  services.envfs = {
+    enable = true;
+
+    extraFallbackPathCommands = ''
+      ln -s ${pkgs.bash}/bin/bash $out/bash
+    '';
+  };
 
   environment.variables = {
     ENVFS_RESOLVE_ALWAYS = "1";
