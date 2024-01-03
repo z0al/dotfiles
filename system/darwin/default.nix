b3a93bd0025793cc97c6cc943905e63911b4300b
z0al <z0al@users.noreply.github.com>
2024-01-03 23:52:18 +0000

    chore(darwin): sort folders first in Finder

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -12,6 +12,11 @@
   ];
 
   system.stateVersion = 4;
+  system.activationScripts.postUserActivation.text = ''
+    # activateSettings -u will reload the settings from the database and apply them to the current session,
+    # so we do not need to logout and login again to make the changes take effect.
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
 
   # User that runs the garbage collector.
   nix.gc.user = user;
--- system/darwin/finder.nix
+++ system/darwin/finder.nix
@@ -1,24 +1,17 @@
 {
-  system.defaults.finder = {
-    # Whether to show icons on the desktop or not. The default is true.
-    CreateDesktop = false;
-
-    # Whether to allow quitting of the Finder. The default is false.
+  # https://macos-defaults.com/finder/
+  system.defaults.CustomUserPreferences."com.apple.finder" = {
+    # Add a quit menu option to the Finder.
     QuitMenuItem = true;
 
-    # Whether to always show hidden files. The default is false.
-    AppleShowAllFiles = true;
-
-    # Whether to always show file extensions. The default is false.
+    # Show all file extensions in the Finder.
     AppleShowAllExtensions = true;
 
-    # Show status bar at bottom of finder windows with item/disk space
-    # stats. The default is false.
-    ShowStatusBar = true;
+    # Show hidden files in the Finder. Use 'cmd+shift+.' to toggle.
+    AppleShowAllFiles = true;
 
-    # Whether to show the full POSIX filepath in the window title. The
-    # default is false.
-    _FXShowPosixPathInTitle = false;
+    # Show path bar in the bottom of the Finder windows
+    ShowPathbar = true;
 
     # Change the default finder view.
     # - “icnv” = Icon view (default)
@@ -27,20 +20,50 @@
     # - “Flwv” = Gallery View
     FXPreferredViewStyle = "icnv";
 
-    # Change the default search scope. Use “SCcf” to default to current
-    # folder. The default is unset (“This Mac”).
+    # Keep folders on top when sorting by name
+    _FXSortFoldersFirst = true;
+
+    # Set the default search scope when performing a search. Use “SCcf” to
+    # default to current folder. The default is unset (“This Mac”).
     FXDefaultSearchScope = "SCcf";
 
-    # Whether to show warnings when change the file extension of
-    # files. The default is true.
-    FXEnableExtensionChangeWarning = false;
-  };
+    # Automatically empty bin after 30 days
+    FXRemoveOldTrashItems = true;
 
-  d.hm = [{
-    targets.darwin.currentHostDefaults = {
-      "com.apple.finder" = {
-        DisableAllAnimations = true;
-      };
-    };
-  }];
+    # Choose whether to display a warning when changing a file extension.
+    FXEnableExtensionChangeWarning = false;
+
+    # Choose whether the default file save location is on disk or iCloud
+    NSDocumentSaveNewDocumentsToCloud = false;
+
+    # Always show folder icon before title in the title bar
+    showWindowTitlebarIcons = true;
+
+    # Show status bar at bottom of finder windows with item/disk space
+    # stats. The default is false.
+    ShowStatusBar = false;
+
+    # Whether to show the full POSIX filepath in the window title. The
+    # default is false.
+    _FXShowPosixPathInTitle = false;
+
+    # Desktop
+    # Whether to show icons on the desktop or not.
+    CreateDesktop = false;
+
+    # Keep folders on top when sorting
+    _FXSortFoldersFirstOnDesktop = true;
+
+    # Show hard disks on desktop
+    ShowHardDrivesOnDesktop = false;
+
+    # Hide external disks on desktop
+    ShowExternalHardDrivesOnDesktop = false;
+
+    # Hide removable media (CDs, DVDs and iPods) on desktop
+    ShowRemovableMediaOnDesktop = false;
+
+    # Show connected servers on desktop
+    ShowMountedServersOnDesktop = false;
+  };
 }
