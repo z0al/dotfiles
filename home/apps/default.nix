df9320b3d49d31708b5e67c72a5e8d1201eaf7e2
z0al <z0al@users.noreply.github.com>
2024-08-04 20:30:10 +0100

    chore: remove old modules

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -2,13 +2,5 @@
   imports = [
     ./helix
     ./rofi
-
-    ./flameshot.nix
-    ./gimp.nix
-    ./inkscape.nix
-    ./obs-studio.nix
-    ./obsidian.nix
-    ./slack.nix
-    ./zoom.nix
   ];
 }
