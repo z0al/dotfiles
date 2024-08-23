dcddd2be4eedd22bf6a91a7d726851196e8c29bd
z0al <z0al@users.noreply.github.com>
2024-08-23 23:10:21 +0100

    chore(wezterm): set cursor style

--- modules/programs/wezterm/settings.nix
+++ modules/programs/wezterm/settings.nix
@@ -28,7 +28,7 @@ in
       color_scheme = cfg.theme;
 
       # Cursor
-      default_cursor_style = "BlinkingBlock";
+      default_cursor_style = "BlinkingBar";
       cursor_blink_ease_in = "Constant";
       cursor_blink_ease_out = "Constant";
       cursor_blink_rate = 500;
