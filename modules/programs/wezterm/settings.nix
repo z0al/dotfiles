59cc2f10a56b9ec5b2eb0069d761e216e29f5703
z0al <z0al@users.noreply.github.com>
2024-08-24 14:42:02 +0100

    chore: revert cursor style
    This reverts commit dcddd2be4eedd22bf6a91a7d726851196e8c29bd.

--- modules/programs/wezterm/settings.nix
+++ modules/programs/wezterm/settings.nix
@@ -28,7 +28,7 @@ in
       color_scheme = cfg.theme;
 
       # Cursor
-      default_cursor_style = "BlinkingBar";
+      default_cursor_style = "BlinkingBlock";
       cursor_blink_ease_in = "Constant";
       cursor_blink_ease_out = "Constant";
       cursor_blink_rate = 500;
