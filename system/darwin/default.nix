7be9961e63b20d63ad2ab0d191f3264a9f0a901e
z0al <z0al@users.noreply.github.com>
2024-09-22 16:54:57 +0100

    chore: bump darwin stateVersion

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -11,7 +11,7 @@
     ./trackpad.nix
   ];
 
-  system.stateVersion = 4;
+  system.stateVersion = 5;
   system.activationScripts.postUserActivation.text = ''
     # activateSettings -u will reload the settings from the database and apply them to the current session,
     # so we do not need to logout and login again to make the changes take effect.
