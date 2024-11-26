da8b9b87ff63a5caeffa8aaf1d4048a510c417d2
z0al <z0al@users.noreply.github.com>
2024-11-26 13:23:32 +0000

    feat(docker): lazily start colima

--- modules/programs/docker.nix
+++ modules/programs/docker.nix
@@ -3,7 +3,31 @@
 let
   cfg = config.d.programs.docker;
 
-  colimaHome = "${config.my.user.xdg.configHome}/colima";
+  cfgHome = config.my.user.xdg.configHome;
+
+  wrapped = with pkgs.unstable;
+    let
+      runtime = lib.getExe colima;
+      docker = lib.getExe docker-client;
+
+      wrapper = writeShellScriptBin "docker" ''
+        set -uo pipefail
+
+        { err=$(exec ${docker} "$@" 2>&1 1>&$out); } {out}>&1
+
+        if [[ $err =~ "Is the docker daemon running?" ]]; then
+          ${runtime} start --cpu 8 --memory 12 --disk 120 &> /dev/null
+          exec ${docker} "$@"
+        fi
+      '';
+    in
+    symlinkJoin {
+      name = "docker";
+      paths = [
+        wrapper
+        docker-client
+      ];
+    };
 in
 
 {
@@ -17,18 +41,13 @@ in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs.unstable; [
       colima
-      docker-client
+      wrapped
       docker-compose
       docker-credential-helpers
-
-      # docker-up command
-      (pkgs.writeScriptBin "docker-up" ''
-        ${lib.getExe colima} start --cpu 8 --memory 12 --disk 120
-      '')
     ];
 
     environment.variables = {
-      DOCKER_HOST = "unix://${colimaHome}/default/docker.sock";
+      DOCKER_HOST = "unix://${cfgHome}/colima/default/docker.sock";
     };
   };
 }
