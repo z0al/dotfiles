4f22458fe1e9862367406cbed323aefbe803caf5
z0al <z0al@users.noreply.github.com>
2024-12-01 14:31:34 +0000

    chore(darwin): enable linux builder

--- hosts/darwin/macbook.nix
+++ hosts/darwin/macbook.nix
@@ -9,4 +9,16 @@
     obsidian.enable = true;
     openscad.enable = true;
   };
+
+  # Required to run `up vm`
+  nix.linux-builder = {
+    enable = true;
+    ephemeral = true;
+    supportedFeatures = [
+      "benchmark"
+      "big-parallel"
+      "kvm"
+      "nixos-test"
+    ];
+  };
 }
