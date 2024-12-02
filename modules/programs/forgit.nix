ebb61073d0a3d605199540e0e1612c5a1433369c
z0al <z0al@users.noreply.github.com>
2024-12-02 21:51:34 +0000

    chore(forgit): remove custom aliasing

--- modules/programs/forgit.nix
+++ modules/programs/forgit.nix
@@ -16,30 +16,5 @@ in
     d.programs.fish.plugins = with pkgs.fishPlugins; [
       forgit
     ];
-
-    environment.variables = {
-      FORGIT_NO_ALIASES = "1";
-    };
-
-    environment.shellAliases = {
-      ga = "git-forgit add";
-      gbd = "git-forgit branch_delete";
-      gbl = "git-forgit blame";
-      gcb = "git-forgit checkout_branch";
-      gcf = "git-forgit checkout_file";
-      gcl = "git-forgit clean";
-      gco = "git-forgit checkout_commit";
-      gcp = "git-forgit cherry_pick_from_branch";
-      gct = "git-forgit checkout_tag";
-      gd = "git-forgit diff";
-      gfu = "git-forgit fixup";
-      gi = "git-forgit ignore";
-      gl = "git-forgit log";
-      grb = "git-forgit rebase";
-      grc = "git-forgit revert_commit";
-      grh = "git-forgit reset_head";
-      gsp = "git-forgit stash_push";
-      gss = "git-forgit stash_show";
-    };
   };
 }
