ea185642e6c988b147822944a88fc60f7bb6f579
z0al <z0al@users.noreply.github.com>
2024-12-02 20:09:26 +0000

    fix: environment.variables -> sessionVariables
    variables are only set interactive where sessionVars are set via PAM

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -12,7 +12,7 @@
     useUserPackages = true;
   };
 
-  environment.variables = {
+  environment.sessionVariables = {
     XDG_CACHE_HOME = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME = "$HOME/.local/share";
