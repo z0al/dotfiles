ab6b7711f85765de86c3774532eb891692a715bf
z0al <12673605+z0al@users.noreply.github.com>
2024-12-03 21:24:49 +0000

    fix(darwin): get the post activation script to execute

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -11,7 +11,7 @@
   ];
 
   system.stateVersion = 5;
-  system.activationScripts.postUserActivation.text = ''
+  system.activationScripts.postActivation.text = ''
     # activateSettings -u will reload the settings from the database and apply them to the current session,
     # so we do not need to logout and login again to make the changes take effect.
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
