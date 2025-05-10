04a7c7fa30f247162a11edbfdbc530ac1f77e5ea
z0al <12673605+z0al@users.noreply.github.com>
2025-05-10 01:11:48 +0100

    chore(darwin): merge misc config

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -14,4 +14,34 @@ in
   ];
 
   system.stateVersion = 5;
+
+  # Random settings that don't fit anywhere else
+  system.defaults.CustomUserPreferences = {
+    "com.apple.controlcenter" = {
+      BatteryShowPercentage = true;
+    };
+
+    # Require password immediately after sleep or screen saver begins
+    "com.apple.screensaver" = {
+      askForPassword = 1;
+      askForPasswordDelay = 0;
+    };
+
+    "com.apple.loginwindow" = {
+      # Allow users to login to the machine as guests using the Guest
+      # account. Default is true.
+      GuestEnabled = false;
+
+      # Hides the Restart button on the login screen. Default is false.
+      RestartDisabled = true;
+
+      # Hides the Shut Down button on the login screen. Default is false.
+      ShutDownDisabled = true;
+
+      # If set to true, the Power Off menu item will be disabled when the
+      # user is logged in. Default is false.
+      PowerOffDisabledWhileLoggedIn = true;
+
+    };
+  };
 }
--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -2,15 +2,6 @@
   imports = [
     ../.
     ./finder.nix
-    ./login.nix
     ./pkgs.nix
   ];
-
-  my.hm.config = {
-    targets.darwin.currentHostDefaults = {
-      "com.apple.controlcenter" = {
-        BatteryShowPercentage = true;
-      };
-    };
-  };
 }
