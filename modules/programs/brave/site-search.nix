86a0610a86f6504b909bc34fa5456acef0188e4c
z0al <12673605+z0al@users.noreply.github.com>
2025-05-12 12:16:58 +0100

    fix(brave): malformed site search urls

--- modules/programs/brave/site-search.nix
+++ modules/programs/brave/site-search.nix
@@ -34,49 +34,49 @@ in
         {
           name = "Nix Packages";
           shortcut = "pkgs";
-          url = "https://search.nixos.org/packages?channel=unstable&query=%s";
+          url = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";
         }
 
         {
           name = "NixOS Options";
           shortcut = "opts";
-          url = "https://search.nixos.org/options?channel=unstable&query=%s";
+          url = "https://search.nixos.org/options?channel=unstable&query={searchTerms}";
         }
 
         {
           name = "Nix Darwin Options";
           shortcut = "dar";
-          url = "https://searchix.ovh/options/darwin/search?query=%s";
+          url = "https://searchix.ovh/options/darwin/search?query={searchTerms}";
         }
 
         {
           name = "Home Manager Options";
           shortcut = "hm";
-          url = "https://searchix.ovh/options/home-manager/search?query=%s";
+          url = "https://searchix.ovh/options/home-manager/search?query={searchTerms}";
         }
 
         {
           name = "GitHub";
           shortcut = "g";
-          url = "https://github.com/search?q=%s&ref=opensearch";
+          url = "https://github.com/search?q={searchTerms}&ref=opensearch";
         }
 
         {
           name = "Nixpkgs GitHub";
           shortcut = "gpkgs";
-          url = "https://github.com/search?q=repo%3ANixOS%2Fnixpkgs%20%s&type=code";
+          url = "https://github.com/search?q=repo%3ANixOS%2Fnixpkgs%20{searchTerms}&type=code";
         }
 
         {
           name = "Nix Darwin GitHub";
           shortcut = "gdar";
-          url = "https://github.com/search?q=repo%3Anix-darwin%2Fnix-darwin%20%s&type=code";
+          url = "https://github.com/search?q=repo%3Anix-darwin%2Fnix-darwin%20{searchTerms}&type=code";
         }
 
         {
           name = "Home Manager GitHub";
           shortcut = "ghm";
-          url = "https://github.com/search?q=repo%3Anix-community%2Fhome-manager%20%s&type=code";
+          url = "https://github.com/search?q=repo%3Anix-community%2Fhome-manager%20{searchTerms}&type=code";
         }
       ];
     };
