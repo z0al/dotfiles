3dd5db9866d1ed14743087945c10d00d84825252
z0al <12673605+z0al@users.noreply.github.com>
2026-04-04 18:22:35 +0100

    fix(zed): install missing lsp servers

--- modules/programs/zed/extensions.nix
+++ modules/programs/zed/extensions.nix
@@ -1,15 +1,33 @@
 {
-  my.programs.zed.settings = {
-    auto_install_extensions = {
-      nix = true;
-      poimandres = true;
-      charmed-icons = true;
-    };
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
-    # Nix
-    languages.Nix.formatter.external = {
-      command = "nixfmt";
-      arguments = [ "--quiet" ];
+let
+  cfg = config.my.programs.zed;
+in
+
+{
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      nixd
+      nil
+    ];
+
+    my.programs.zed.settings = {
+      auto_install_extensions = {
+        nix = true;
+        poimandres = true;
+        charmed-icons = true;
+      };
+
+      # Nix
+      languages.Nix.formatter.external = {
+        command = "nixfmt";
+        arguments = [ "--quiet" ];
+      };
     };
   };
 }
