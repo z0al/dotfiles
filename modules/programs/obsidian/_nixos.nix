8ca4a8044a50417323552c3317869884e30f4712
z0al <12673605+z0al@users.noreply.github.com>
2026-09-24 20:25:55 +0100

    fix(obsidian): recursive config resolution

--- modules/programs/obsidian/_nixos.nix
+++ modules/programs/obsidian/_nixos.nix
@@ -1,16 +1,8 @@
 {
-  config,
   pkgs,
-  lib,
   ...
 }:
 
-let
-  cfg = config.my.programs.obsidian;
-in
-
 {
-  config = lib.mkIf cfg.enable {
-    my.programs.obsidian.package = pkgs.obsidian;
-  };
+  my.programs.obsidian.package = pkgs.obsidian;
 }
