041ac9ed3d7d27a18cab7412e603ca10ad24200b
z0al <12673605+z0al@users.noreply.github.com>
2026-09-24 15:57:46 +0100

    chore: increase linux builder disk size

--- modules/config/nix/_darwin.nix
+++ modules/config/nix/_darwin.nix
@@ -1,6 +1,9 @@
 {
   # Build aarch64-linux guests on Apple Silicon.
-  nix.linux-builder.enable = true;
+  nix.linux-builder = {
+    enable = true;
+    config.virtualisation.darwin-builder.diskSize = 100 * 1024;
+  };
 
   nix.gc = {
     interval.Day = 7;
